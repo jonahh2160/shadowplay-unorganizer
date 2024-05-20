c4721a33e9c826b00e9c29f20d7a0ec7c8bf049a
jonahh2160 <144245615+jonahh2160@users.noreply.github.com>
2024-05-19 20:40:36 -0500

    Finished the script

--- README.md
+++ README.md
@@ -4,7 +4,24 @@ Python script to move video files to one place and delete empty folders.
 
 ## Description
 
-Nvidia's Instant Replay function automatically sorts replays into folders. This Python script "unorganizes" everything, moving all of the clips to one place. Good for viewing everything by date.
-I made this for personal use, but feel free to use it how you'd like.
+Nvidia's Instant Replay function automatically sorts replays into folders. This Python script "unorganizes" everything, moving all of the clips to one place. Good for viewing everything by date or chronologically.
+
+By default it'll dump everything into the same directory that the script is in, but if you want to move the files to another directory, you can just edit the .yaml config file. It also has the ability to omit folders from the unorganizing process.
+
+I made this for personal use, but feel free to use it how you'd like, or if you need a starting point for managing your own files.
 
 ## Usage
+
+> Install [Python](https://www.python.org/downloads/) if you haven't already
+
+> Place the script into the directory you want to unorganize
+
+> *Optional: move the `unorganizer_config.yaml` into the same directory and edit its contents*
+
+> Open a terminal in the same directory (you can do this by clicking the file path, deleting the selection, and typing `cmd`)
+
+> In the terminal, type `python sp_unorganizer.py`
+
+> Let the script run
+
+> Done!
---
+++ sp_unorganizer.py
@@ -0,0 +1,48 @@
+# Import packages
+import os, yaml, shutil, send2trash
+from collections import deque
+
+# Read config from file
+with open("unorganizer_config.yaml", "r") as stream:
+    try:
+        # Store config into a dict
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        # Print error
+        print(exc)
+
+# Set config variables
+target_directory = config["target_directory"]
+exempt_folders = config["exempt_folders"]
+
+# Get the current directory and create some deques
+current_directory = os.path.dirname(os.path.realpath(__file__))
+file_queue = deque()
+folder_queue = deque()
+
+# If the target directory is the default or empty, set it to the current_directory
+if target_directory == "/path/to/target/directory" or not target_directory:
+    target_directory = current_directory
+
+# Loop through the directory contents: delete empty folders and add folder contents to a queue
+for folder in os.listdir(current_directory):
+    if os.path.isdir(folder) and folder not in exempt_folders:
+        # If the folder is empty, delete it
+        if not os.listdir(folder):
+            send2trash.send2trash(folder)
+        # Otherwise, add the folder and its contents the folder_queue and the file_queue
+        else:
+            folder_queue.append(folder)
+            current_folder = os.path.join(current_directory, folder)
+            for file in os.listdir(current_folder):
+                file_queue.append(os.path.join(current_folder, file))
+
+# Move the files in the file_queue to the main directory
+while file_queue:
+    file = file_queue.popleft()
+    shutil.move(file, target_directory)
+
+# Move the now emptied folders to the recycling bin
+while folder_queue:
+    folder = folder_queue.popleft()
+    send2trash.send2trash(folder)
